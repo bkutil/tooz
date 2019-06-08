3f6b1ab783ba6db033201fd065ed9be0444cbb5a
Balazs Kutil <balazs@kutilovi.cz>
2019-06-08 20:19:10 +0200

    Přidání vazby mezi modely

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :name, :price, :image, presence: true
   validates :name, :description, length: { maximum: 2048 }
   validates :price, numericality: { greater_than: 0 }
+
+  has_many :order_items, inverse_of: :product
 end
