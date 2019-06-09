13a42ca757a7119c0c5f958101e67453f2163a17
Balazs Kutil <balazs@kutilovi.cz>
2019-06-09 10:26:43 +0200

    Odstraněním testů ušetříme čas při vývoji ztracený jejich spouštěním

--- test/models/order_item_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OrderItemTest < ActiveSupport::TestCase
-  test "#total price" do
-    product = Product.new(name: "Test", price: 1230)
-    item = OrderItem.new(product: product, quantity: 4)
-    assert_equal item.total_price, 4 * 1230
-  end
-end
