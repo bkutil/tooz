eef3b4a0c849f030e900545e699373bcccc29b25
Balazs Kutil <balazs@kutilovi.cz>
2019-06-09 08:24:58 +0200

    Pomocná metoda vytvářející/vracející aktuální košík

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_order
+
+  private
+
+  def current_order
+    @current_order ||= Order.includes(order_items: :product)
+                            .where(id: session[:order_id].to_i)
+                            .first_or_initialize
+  end
 end
